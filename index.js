a2fb5d7144123179644095477772c1947d24ddcc
Anar <ndjd86d5fw@privaterelay.appleid.com>
2026-09-04 18:27:02 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
 require("dotenv").config();
 
-const { Client, GatewayIntentBits } = require("discord.js");
+const { Client, GatewayIntentBits, REST, Routes, SlashCommandBuilder } = require("discord.js");
 
 const client = new Client({
   intents: [GatewayIntentBits.Guilds]
 });
 
-client.once("ready", () => {
+const commands = [
+  new SlashCommandBuilder()
+    .setName("ping")
+    .setDescription("Check if OurBot is online")
+].map(command => command.toJSON());
+
+const rest = new REST({ version: "10" }).setToken(process.env.DISCORD_TOKEN);
+
+client.once("ready", async () => {
   console.log(`OurBot is online as ${client.user.tag}`);
+
+  await rest.put(
+    Routes.applicationCommands(client.user.id),
+    { body: commands }
+  );
+
+  console.log("Slash commands registered.");
 });
 
 client.login(process.env.DISCORD_TOKEN);
