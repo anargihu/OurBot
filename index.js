5879bf1c4c68607de167b2b85f5b06d885113dd4
Anar <ndjd86d5fw@privaterelay.appleid.com>
2026-09-04 17:52:13 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+require("dotenv").config();
+
+const { Client, GatewayIntentBits } = require("discord.js");
+
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds]
+});
+
+client.once("ready", () => {
+  console.log(`OurBot is online as ${client.user.tag}`);
+});
+
+client.login(process.env.DISCORD_TOKEN);
