9e529040e93dfe87fbf5e20b50171dd0475c76f4
Anar <ndjd86d5fw@privaterelay.appleid.com>
2026-09-04 19:36:21 +0800

    Create pings.js

---
+++ Commands/pings.js
@@ -0,0 +1,20 @@
+const { SlashCommandBuilder } = require("discord.js");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("ping")
+    .setDescription("Check if OurBot is online"),
+
+  async execute(interaction, client) {
+    const sent = await interaction.reply({
+      content: "🏓 Pong!",
+      fetchReply: true
+    });
+
+    const latency = sent.createdTimestamp - interaction.createdTimestamp;
+
+    await interaction.editReply(
+      `🏓 Pong! Latency: ${latency}ms | WS: ${client.ws.ping}ms`
+    );
+  }
+};
